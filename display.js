6db4a3d4de41f31a51431d11f2a74d1db5db7a6b
Jhansi <jhansisridevi@gmail.com>
2023-11-06 06:40:23 -0500

    Added modules

---
+++ display.js
@@ -0,0 +1,8 @@
+const searchInput = document.getElementById("search-input");
+const searchInput1 = document.getElementById("search-input1");
+
+export function dateHandler() {
+  const today = new Date().toISOString().split("T")[0];
+  searchInput1.setAttribute("max", today);
+  searchInput.setAttribute("max", today);
+}
