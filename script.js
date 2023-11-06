60ce4f4dda2a000698c5a829a9bb5d0bcf04fc15
Jhansi <jhansisridevi@gmail.com>
2023-11-06 07:14:17 -0500

    Added display function to retrieve fetch reults

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { dateHandler } from "./display.js";
+
 const searchButton = document.getElementById("search-button");
 const searchInput = document.getElementById("search-input");
 const searchInput1 = document.getElementById("search-input1");
@@ -7,11 +9,7 @@ const prevPageButton = document.getElementById("prev-page");
 const nextPageButton = document.getElementById("next-page");
 const pageNumber = document.getElementById("page-number");
 
-const today = new Date().toISOString().split("T")[0]; //to extract current date in YYYY-MM-DD 
-//console.log(new Date());
-searchInput1.setAttribute("max", today);
-searchInput.setAttribute("max", today);
-
+dateHandler();
 let currentPage = 1;
 const itemsPerPage = 3; //limiting number of cards to display
 pagination.style.display = "none"; //to hide previous and next buttons
@@ -19,7 +17,6 @@ pagination.style.display = "none"; //to hide previous and next buttons
 searchButton.addEventListener("click", () => {
   currentPage = 1;
   fetchResults(currentPage);
-  pagination.style.display = "block";
 });
 
 prevPageButton.addEventListener("click", () => {
@@ -34,8 +31,9 @@ nextPageButton.addEventListener("click", () => {
   fetchResults(currentPage);
 });
 
-async function fetchResults(page) {  //query parameter for different search queries
-  
+async function fetchResults(page) {
+  //query parameter for different search queries
+  const apiKey = "kub1bSjX1d0CdMZxAUWiU7BAfRFw7rACVYZsEXXO";
   const apiUrl = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&start_date=${searchInput.value}&end_date=${searchInput1.value}`;
   console.log(searchInput.value);
   console.log(searchInput1.value);
@@ -49,4 +47,51 @@ async function fetchResults(page) {  //query parameter for different search quer
       console.error("Error fetching data:", error);
     });
 }
-function displayResults(data, currentPage){}
+function displayResults(data, currentPage) {
+  cardContainer.innerHTML = "";
+  pagination.style.display = "block";
+  try {
+    if (data && data.length > 0) {
+      const startIndex = (currentPage - 1) * itemsPerPage;
+      const endIndex = startIndex + itemsPerPage;
+      const itemstoDisplay = data.slice(startIndex, endIndex); //list of items to display
+      console.log(itemstoDisplay);
+      for (const i in itemstoDisplay) {
+        const card = document.createElement("div");
+        card.classList.add("card");
+
+        const title = document.createElement("h2");
+        title.textContent = itemstoDisplay[i].title;
+
+        //   const description = document.createElement("p");
+        //   description.textContent = data[i].explanation;
+
+        if (itemstoDisplay[i].media_type === "video") {
+          const video = document.createElement("iframe");
+          video.src = itemstoDisplay[i].url;
+          card.appendChild(video);
+        } else if (itemstoDisplay[i].media_type === "image") {
+          const image = document.createElement("img");
+          image.src = itemstoDisplay[i].hdurl;
+          card.appendChild(image);
+        }
+
+        card.appendChild(title);
+        //   card.appendChild(description);
+        cardContainer.appendChild(card);
+      }
+      const totalResults = data.length;
+      console.log(totalResults);
+      const totalPages = Math.ceil(totalResults / itemsPerPage); //to determine total number of pages 
+      pageNumber.textContent = currentPage;
+
+      prevPageButton.disabled = currentPage === 1;
+      nextPageButton.disabled = currentPage === totalPages;
+    } else {
+      pagination.style.display = "none";
+      cardContainer.innerHTML = "No results found.";
+    }
+  } catch (error) {
+    console.log("Error", error);
+  }
+}
